50d278bfba4272fc989610422c9aa41ffd26c1b6
hkamatsuka <hiroshi_k2k@nifty.com>
2016-01-03 22:39:46 +0800

    update

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 apt-get -y update
 apt-get -y upgrade
-cd /home/pi
-mkdir -p shell/Python shell/log
 
+cd /home/pi
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 pip install awscli
@@ -14,32 +13,32 @@ wget https://pypi.python.org/packages/source/s/simplejson/simplejson-3.8.1.tar.g
 tar -xvzf simplejson*.tar.gz
 cd simplejson*
 python setup.py install
-cd ../
 
+cd /home/pi
 wget https://pypi.python.org/packages/source/r/requests/requests-2.9.0.tar.gz
 tar -xvzf requests-2.9.0.tar.gz
 cd requests-2.9.0
 python setup.py install
-cd ../
 
+cd /home/pi
 wget https://pypi.python.org/packages/source/r/requests-oauthlib/requests-oauthlib-0.6.0.tar.gz
 tar -xvzf requests-oauthlib-0.6.0.tar.gz
 cd requests-oauthlib-0.6.0
 python setup.py install
-cd ../
  
+cd /home/pi
 wget https://pypi.python.org/packages/source/s/setuptools/setuptools-19.1.1.tar.gz
 tar -xvzf setuptools-19.1.1.tar.gz
 cd setuptools*
 python setup.py install
-cd ../
 
+cd /home/pi
 wget https://pypi.python.org/packages/source/o/oauthlib/oauthlib-1.0.3.tar.gz
 tar -xvzf oauthlib-1.0.3.tar.gz
 cd oauthlib-*
 python setup.py install
-cd ../
 
+cd /home/pi
 wget https://python-twitter.googlecode.com/files/python-twitter-1.1.tar.gz
 tar -xvzf python-twitter-1.1.tar.gz
 cd python-twitter-*
@@ -47,29 +46,14 @@ python setup.py install
 
 apt-get -y install chkconfig
 apt-get -y install i2c-tools python-smbus
+apt-get -y install tightvncserver
 
-cat >> /etc/modules <<EOF
-i2c-bcm2708 
-i2c-dev
-EOF
-
-cat >> /boot/config.txt <<EOF
-dtparam=i2c_arm=on
-EOF
-
-cd /home/pi
-cp -Rp ./pi_root/* /
+mkdir -p /home/pi/shell/Python /home/pi/shell/log
+cp -Rp /home/pi/pi_root/* /
 
 update-rc.d vncboot defaults
 update-rc.d sensor01_02.sh defaults
 update-rc.d sensor03.sh defaults
 update-rc.d sensor04.sh defaults
 
-cat >> cron << EOF
-*/5 * * * * /home/pi/shell/Python/FileUpload_01.py > /dev/null 2>&1
-*/5 * * * * /home/pi/shell/Python/FileUpload_02.py > /dev/null 2>&1
-*/5 * * * * /home/pi/shell/Python/FileUpload_03.py > /dev/null 2>&1
-*/5 * * * * /home/pi/shell/Python/FileUpload_04.py > /dev/null 2>&1
-EOF
-
-crontab cron
+crontab /var/spool/cron/crontabs/root
