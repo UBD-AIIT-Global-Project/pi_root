0824d3129c4b626aedc01677636b4b8c89ba460b
hkamatsuka <hiroshi_k2k@nifty.com>
2016-01-16 10:28:03 +0800

    add

--- home/pi/shell/Python/FileCreate_03.py
+++ home/pi/shell/Python/FileCreate_03.py
@@ -8,16 +8,23 @@ import time
 import RPi.GPIO as GPIO
 from hc_sr04 import hc_sr04
 
+from  TwitterSend  import  TwitterSend
+tw = TwitterSend()
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 ultrasound = hc_sr04(11,13)
 logdir="/home/pi/shell/log/"
+file_Threshold = logdir + "threshold_03"
+f_Threshold = open(file_Threshold,"r")
+for line in f_Threshold:
+  threshold = line
 
 while True:
   today = datetime.now().strftime("%Y%m%d")
   ctime = datetime.now().strftime("%H%M")
   file_Usonic = logdir + today + "_03_SENSOR.log"
-  f_Usonic = open(file_Usonic, "w")
+  f_Usonic = open(file_Usonic, "a")
   cnt = 0
   while True:
     cnt += 1
@@ -25,5 +32,13 @@ while True:
       break
     distance = ultrasound.measure()
     distance = "%08d" % (distance)
+    print distance
+    if distance <= threshold:
+      tw = TwitterSend()
+      msg = today + " " + ctime + " " + str(cnt) + " Warning !! This place fladded. Evacuate from here ASAP"
+      print msg
+      tw.setMsg(msg)
+      tw.sendMsg()
+
     f_Usonic.write(today + ctime + "," + distance + "\n") 
     time.sleep(1)
