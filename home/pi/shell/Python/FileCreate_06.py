f97e46dc898e14431cd9da20a7f95e0bdcc33c50
hkamatsuka <hiroshi_k2k@nifty.com>
2016-02-24 23:14:39 +0800

    update

--- home/pi/shell/Python/FileCreate_06.py
+++ home/pi/shell/Python/FileCreate_06.py
@@ -22,7 +22,9 @@ print threshold
 
 while True:
   today = datetime.now().strftime("%Y%m%d")
+  today2 = datetime.now().strftime("%Y/%m/%d")
   ctime = datetime.now().strftime("%H%M")
+  ctime2 = datetime.now().strftime("%H:%M")
   file_Usonic = logdir + today + "_06_SENSOR.log"
   #f_Usonic = open(file_Usonic, "a")
   cnt = 0
@@ -31,23 +33,27 @@ while True:
     cnt += 1
     if cnt == 60:
       break
-    distance1 = ultrasonicRead(ultrasonic_ranger)
+    try:
+      distance1 = ultrasonicRead(ultrasonic_ranger)
+    except:
+      distance1 = 999
     distance2 = "%08d" % (distance1)
     print distance1
     f_Usonic = open(file_Usonic, "a")
     f_Usonic.write(today + ctime + "," + distance2 + "\n") 
     f_Usonic.close()
-    if distance1 <= threshold:
+    if distance1:
+     if distance1 <= threshold:
       cnt2 = cnt2 + 1
-      if cnt2 >= 3:
+      if (cnt2 == 3) or ((cnt2 > 3) and (cnt2 % 30 == 0)):
         try:
-          msg = today + " " + ctime + " " + str(cnt) + " Warning !! AIIT is floodded. Evacuate from here ASAP"
+          msg = today2 + " " + ctime2 + " " + str(cnt) + " Warning !! AIIT is floodded. Evacuate from here ASAP"
           print msg
           tw.setMsg(msg)
           tw.sendMsg()
         except:
           print "twitter error" 
-    else:
-      cnt2 = 0  
+     else:
+      cnt2 = 0
 
     time.sleep(1)
